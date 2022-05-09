0c9fdf188331b26e23c012c42fe7c0ead91eebff
franjribatto <80211117+frannjribatto@users.noreply.github.com>
2022-05-08 22:15:36 -0300

    Ejercicio completo

--- dwf-mod-2/TypeScript & Git/cosas.ts
+++ dwf-mod-2/TypeScript & Git/cosas.ts
@@ -1,5 +1,30 @@
-function getAll() {}
+const listaDeCosas = [
+  {
+    product: "Heladera",
+    id: 1,
+    tags: ["electrodomestico", "freezer", "ahorro de energia"],
+  },
+  {
+    product: "Computadura",
+    id: 2,
+    tags: ["electrodomestico", "gamer", "didactico"],
+  },
+  {
+    product: "Equipo de musica",
+    id: 3,
+    tags: ["electrodomestico", "musica", "entretenimiento"],
+  },
+];
 
-function getByID(id: number) {}
+function getAll() {
+  const product = listaDeCosas;
+  return product;
+}
+
+function getByID(id: number) {
+  const product = listaDeCosas.filter((objeto) => objeto.id == id);
+
+  return product;
+}
 
 export { getAll, getByID };
--- dwf-mod-2/TypeScript & Git/index.ts
+++ dwf-mod-2/TypeScript & Git/index.ts
@@ -1,5 +1,9 @@
 import { getAll, getByID } from "./cosas";
 
-function main() {}
+function main() {
+  console.table(getAll());
+  console.log("\n");
+  console.table(getByID(2));
+}
 
 main();
