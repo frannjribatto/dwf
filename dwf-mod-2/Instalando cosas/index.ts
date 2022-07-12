bfbad18f5fe4199139c3694e1406e28e87873d39
franjribatto <80211117+frannjribatto@users.noreply.github.com>
2022-07-11 22:15:43 -0300

    Actualizacion de Proyecto

--- dwf-mod-2/Instalando cosas/index.ts
+++ dwf-mod-2/Instalando cosas/index.ts
@@ -1,10 +1,9 @@
-var nombre:string = "Carolina Jimena Paz"
-
+var nombre: string = "Carolina Jimena Paz";
 
 function main() {
   mostrarNombre(nombre);
 }
-  
-function mostrarNombre(nombre:string) {
-  console.log(nombre);  
+
+function mostrarNombre(nombre: string) {
+  console.log(nombre);
 }
