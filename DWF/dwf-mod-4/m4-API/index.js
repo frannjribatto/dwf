4c51ca33a85eaed542a49ae9590c83e967d8be04
Franco Ribatto <frannj.ribatto@gmail.com>
2023-10-29 19:52:55 -0300

    Actualizacion

---
+++ DWF/dwf-mod-4/m4-API/index.js
@@ -0,0 +1,10 @@
+function main(){
+const formEl = document.querySelector(".search-form");
+formEl.addEventListener("submit", function(e){
+    e.preventDefault()
+    const itemSearch = e.target.buscar.value;
+    fetch
+})
+}
+
+main();
